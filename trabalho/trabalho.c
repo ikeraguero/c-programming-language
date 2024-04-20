3ea2b60dbcc3d1246c8fb6be644310ba61aac5f1
ikeraguero <ikerpires407@gmail.com>
2024-04-20 12:37:54 -0300

    Add Homework First Commit

---
+++ trabalho/trabalho.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    printf("======================================\n");
+    printf("Bem-vindo ao Gerenciador de Conjuntos!\n");
+    printf("======================================\n");
+    printf("\n");
+    printf("Selecione a opção que deseja: \n");
+    printf("\n");
+    printf("[1] - Criar um novo conjunto vazio \n");
+    printf("[2] - Inserir dados em um conjunto \n");
+    printf("[3] - Remover um conjunto \n");
+    printf("[4] - Fazer união entre dois conjuntos \n");
+    printf("[5] - Fazer a intersecção entre dois conjuntos \n");
+    printf("[6] - Mostrar um conjunto \n");
+    printf("[7] - Mostrar todos os conjuntos \n");
+    printf("[8] - Buscar por um valor \n");
+    printf("[9] - Sair do programa \n");
+
+
+
+
+
+
+}
