4f3ecb08390cf40390ece2661c268ea53c8e387d
ikeraguero <ikerpires407@gmail.com>
2024-06-02 23:54:31 -0300

    Remove Book

--- book/chapter 1/ex2.c
+++
@@ -1,8 +0,0 @@
-/*Exercise 1-2. Experiment to find out what happens when prints's argument string contains
-\c, where c is some character not listed above.*/
-
-#include <stdio.h>
-
-int main() {
-    printf("Hello World \c");
-}
